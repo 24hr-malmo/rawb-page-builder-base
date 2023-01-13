dd4c3fa5b994a4b8b762887bc5ae43dfb8974841
cchaglund <cchaglund@users.noreply.github.com>
2023-01-13 13:20:09 +0000

    Add Built Files

--- build/blocks.asset.php
+++ build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'f98eba6748ffc1594f1e');
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '68ba1c70b517a0ae21c0');
